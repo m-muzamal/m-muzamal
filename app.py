98803e93385c48748c1221897ff345677cc272d7
m-muzamal <muzamal503@gmail.com>
2025-01-05 22:35:01 +0500

    updated

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import os
 from random import randint 
 
-# Loop for the dates from January 1, 2025, to January 4, 2025
-for day in range(1, 5):  # 1 to 4
-    date_str = f"2025-01-{day:02d}"  # Format as YYYY-MM-DD
-    
-    for j in range(0, randint(1, 10)):  # Random number of commits per day
-        d = f"{date_str} days ago"  # Create the commit date string
+for i in range(1, 365):
+
+    for j in range(0, randint(1, 10)):
+        d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
-            file.write(d + '\n')  # Append to file with a newline after each entry
-
+            file.write(d)
         os.system('git add .')
-        os.system(f'git commit --date="{d}" -m "update"')
+        os.system('git commit --date="'+ d +'" -m "update"')
 
-# Finally push all changes
 os.system('git push -u origin main')
